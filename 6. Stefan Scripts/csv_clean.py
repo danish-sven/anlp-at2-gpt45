d734d2c281561ee8fdcbcb34d18aa395b3e743bd
Stefan <stefan.c@ridezoomo.com>
2023-04-22 12:21:32 +1000

    Repo Clean

--- 6. Stefan Scripts/csv_clean.py
+++ 6. Stefan Scripts/csv_clean.py
@@ -1,17 +1,48 @@
+"""
+This script reads multiple CSV files containing sentence pairs (Original and Corrected)
+from a specified folder (CSV). It calculates the Levenshtein distance and cosine similarity
+between the Original and Corrected sentences. The script then filters the pairs based on
+minimum row count and writes the resulting DataFrame to a new CSV file in the 'training' folder.
+
+Usage:
+    Place the input CSV files in the 'CSV' folder and run the script.
+"""
+
 import os
 import pandas as pd
 from Levenshtein import distance as levenshtein_distance
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 def calculate_cosine_similarity(text1, text2):
+    """
+    Calculate the cosine similarity between two text strings.
+
+    Args:
+        text1 (str): The first text string.
+        text2 (str): The second text string.
+
+    Returns:
+        float: The cosine similarity between the two text strings.
+    """
     try:
         vectorizer = TfidfVectorizer().fit_transform([text1, text2])
         return cosine_similarity(vectorizer)[0][1]
     except ValueError:
         return 0
 
+
 def calculate_metrics(row):
+    """
+    Calculate the Levenshtein distance and cosine similarity for a DataFrame row.
+
+    Args:
+        row (pd.Series): A row from a DataFrame containing 'Original' and 'Corrected' columns.
+
+    Returns:
+        pd.Series: A new row with 'levenshtein' and 'cosine' columns.
+    """
     original = row['Original']
     corrected = row['Corrected']
 
@@ -23,6 +54,7 @@ def calculate_metrics(row):
         cos = 1
 
     return pd.Series({'levenshtein': lev, 'cosine': cos})
+
 
 csv_folder = 'CSV'
 training_folder = 'training'
@@ -36,7 +68,7 @@ if not os.path.exists(training_folder):
 for file in os.listdir(csv_folder):
     if file.endswith('.csv'):
         file_path = os.path.join(csv_folder, file)
-        
+
         df = pd.read_csv(file_path)
 
         # Check if the necessary columns exist in the DataFrame
@@ -50,8 +82,6 @@ for file in os.listdir(csv_folder):
             continue
 
         df[['levenshtein', 'cosine']] = df.apply(calculate_metrics, axis=1)
-
-        # filtered_df = df[(df['levenshtein'] >= threshold_levenshtein) | (df['cosine'] <= threshold_cosine)]
 
         if len(df) >= min_rows:
             new_file_path = os.path.join(training_folder, file)
--- 6. Stefan Scripts/utils.py
+++
@@ -1,9 +0,0 @@
-import langid
-
-def lang_check(text, lang):
-    predicted_lang, prob = langid.classify(text)
-
-    if predicted_lang == lang:
-        return True
-    else:
-        return False
